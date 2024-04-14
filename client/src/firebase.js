269155b100d549df994c12082d893fac291aa00f
Salilyyyy <hgiang140302@gmail.com>
2024-04-14 11:04:31 +0700

    update

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,16 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyARoL1HnWzDMF_rSivY5CWJNZLpp6ifSic',
-  authDomain: 'lalsasia.firebaseapp.com',
-  projectId: 'lalsasia',
-  storageBucket: 'lalsasia.appspot.com',
-  messagingSenderId: '228627684040',
-  appId: '1:228627684040:web:0afe321804f99108d323fd',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export default app;
